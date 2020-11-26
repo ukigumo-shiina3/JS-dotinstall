fce51aafc25a744687161c2365e9f6f77d5d7d2a
Daisuke Tanaka <daisukeyao@outlook.jp>
2020-11-26 19:57:33 +0900

    [Add]basic_grammar

--- app/views/layouts/js/basic.js
+++ app/views/layouts/js/basic.js
@@ -1,3 +1,12 @@
 'use strict';
 
-console.log('Hello World from basic.js!');
+// console.log('hello');
+// console.log("hello");
+
+// console.log('it's me!');
+// console.log("it's me!");
+// console.log('it\'s me!');
+
+// console.log('hel\nlo wor\tld');
+
+console.log('hello' + 'world');
