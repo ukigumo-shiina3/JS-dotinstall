0fb8a24dc4230baad255f9fc0b567804fdb778bc
Daisuke Tanaka <daisukeyao@outlook.jp>
2020-11-28 21:29:10 +0900

    [Add]basic_grammar

--- app/views/layouts/js/basic.js
+++ app/views/layouts/js/basic.js
@@ -1,12 +1,35 @@
 'use strict';
 
-// console.log('hello');
-// console.log("hello");
+// let price = 150;
+// console.log(price * 140);
+// console.log(price * 160);
 
-// console.log('it's me!');
-// console.log("it's me!");
-// console.log('it\'s me!');
+// price = 170;
+// console.log(price * 140);
+// console.log(price * 160);
 
-// console.log('hel\nlo wor\tld');
+let price = 500;
 
-console.log('hello' + 'world');
+price += 100; //600
+
+price *= 2; //1200
+
+price ++; //1201
+
+price --; //1200
+
+console.log(price)
+
+console.log(typeof 'hello'); //string
+console.log(typeof 5); //number
+console.log(typeof true); //boolean
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object 有名なJSのバグ
+
+console.log('5' * 3); //15
+console.log('5' - 3); //2
+
+console.log('5' + 3) //53 文字列の連結のための演算子として見られる
+console.log(parseInt('5', 10) + 3); //8 文字列を10進数の整数値に変換
+
+console.log(parseInt('hello', 10) + 3);// NaN not a number 数値にしようとしたけどできなかった場合の値
