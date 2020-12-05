93500bbc5c233802e01e04a9d885284d5b8b19a1
Daisuke Tanaka <daisukeyao@outlook.jp>
2020-12-05 22:38:13 +0900

    [Add]basic_grammar

--- app/views/layouts/js/basic.js
+++ app/views/layouts/js/basic.js
@@ -1,17 +1,30 @@
 'use strict';
 
-const price = 1200;
+// const score = 40;
 
-// console.log(price > 1000); // true
-// console.log(price < 1000); // false
-// console.log(price >= 1000); // true
-// console.log(price <= 1000); // false
-// console.log(price === 1000); // false
-// console.log(price !== 1000); // true
+// if (score >= 80) {
+//     console.log('Great!');
+// } else if (score >= 60) {
+//     console.log('Good.');
+// } else {
+//     console.log('OK...');
+// }
 
-// false <- 0, null, undefined, '', false
-// true <- それ以外
 
-console.log(Boolean(0));
-console.log(Boolean('hello'));
 
+const score = 60;
+const name = 'taguchi';
+
+// if (score >= 50) {
+//   if (name === 'taguchi') {
+//     console.log('Good job!');
+//   }
+// }
+
+// && なおかつ（AND）
+// || もしくは（OR）
+// ! 〜ではない（NOT）
+
+if (score >= 50 && name === 'taguchi') {
+    console.log('Good job!');
+}
